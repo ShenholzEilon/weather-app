649fec65cd50ab005fa269f48d2d894e242b0cfd
Eilon Shenholz <eilon.sh@moonactive.com>
2021-09-30 11:03:31 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require("request");
+forecast = (lat, long, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=263676995dd94cd7a13599e37baa8ee4&query=' + lat +',' + long
+    console.log(url)
+    request({url, json: true}, (error, { body } = {})=>{
+        if (error){
+            callback('Unable to connect', undefined)
+        }else if (body.error){
+            callback('Incorrect location', undefined)
+        }else{
+            callback(undefined, {
+                temp: body.current.temperature,
+                feelsLike: body.current.feelslike
+            })
+        }
+    })
+}
+
+module.exports = forecast
